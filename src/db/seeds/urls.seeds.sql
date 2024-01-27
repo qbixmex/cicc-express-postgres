002451b3632ecf57ce0219e833a8bcb76aae4b82
Daniel González Briseño <qbixmex@gmail.com>
2024-01-26 16:56:06 -0800

    migrations-and-seeds-finished

---
+++ src/db/seeds/urls.seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO urls (user_id, original_url, short_url) 
+VALUES
+  (1, 'https://www.npmjs.com/package/node', 'http://localhost:3000/Yr75f2'),
+  (1, 'https://www.npmjs.com/package/express', 'http://localhost:3000/24i9wH'),
+  (1, 'https://www.npmjs.com/package/react', 'http://localhost:3000/yH6V39'),
+  (2, 'https://www.npmjs.com/package/angular', 'http://localhost:3000/pU7V4D'),
+  (3, 'https://www.npmjs.com/package/dotenv', 'http://localhost:3000/Y46Bd4'),
+  (3, 'https://www.npmjs.com/package/vue', 'http://localhost:3000/yg53Bv');
---
+++ src/db/seeds/users.seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, password)
+VALUES
+  ('Daniel Gonzalez', 'daniel@gmail.com', 'supersecretpassword'),
+  ('Fernando Dominguez', 'fernando@gmail.com', 'supersecretpassword'),
+  ('Karla Oliveira', 'karla@gmail.com', 'supersecretpassword');
